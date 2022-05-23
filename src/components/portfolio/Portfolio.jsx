452a544afbd1f6d8d1e1bb05d6b305fa4a73d871
Daiana Michels <daianaem.12@gmail.com>
2022-05-23 15:13:10 -0300

    fix: inicia reparos responsividade

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,6 +3,7 @@ import './portfolio.css'
 import IMG1 from '../../assets/portfolio1.jpg'
 import IMG2 from '../../assets/portfolio2.jpg'
 import IMG3 from '../../assets/portfolio3.png'
+import IMG4 from '../../assets/portfolio4.jpg'
 
 //importar imagem do todo list, sgi e book store
 //por no netlify o todo list e o book store
@@ -11,24 +12,24 @@ const Portfolio = () => {
   const data = [
     {
       id: 1,
-      image: IMG1,
-      title: 'TO-DO List',
-      github: 'https://github.com/DaianaEgerMichels/Lista-de-Afazeres---ToDoList',
-      netlify: 'linknetlify'
-    },
-    {
-      id: 2,
       image: IMG2,
       title: 'SGI',
       github: 'https://github.com/DaianaEgerMichels/projeto-02',
       netlify: 'https://sistema-de-gestao-integrada.netlify.app/'
     },
     {
-      id: 3,
+      id: 2,
       image: IMG3,
       title: 'Book Store',
       github: 'https://github.com/DaianaEgerMichels/redux-book-store',
-      netlify: 'linknetlify'
+      netlify: 'https://book-store-redux.netlify.app/'
+    },
+    {
+      id: 3,
+      image: IMG4,
+      title: 'Conversor de Temperatura',
+      github: 'https://github.com/DaianaEgerMichels/ConversorDeTemperatura',
+      netlify: 'https://conversor-de-temperatura-js.netlify.app/'
     }
 
   ]
@@ -39,6 +40,15 @@ const Portfolio = () => {
       <h2>Portfolio</h2>
 
       <div className="container portfolio__container">
+        <article className='portfolio__item'>
+                <div className="portifolio__item-image">
+                  <img src={IMG1} alt='TO DO List'></img>
+                </div>
+                <h3>TO DO List</h3>
+                <div className="portfolio__item-cta">
+                  <a href="https://github.com/DaianaEgerMichels/Lista-de-Afazeres---ToDoList" className='btn' target='_blank'>GitHub</a>
+                </div>
+        </article>
         {
           data.map(({id, image, title, github, netlify}) => {
             return (
